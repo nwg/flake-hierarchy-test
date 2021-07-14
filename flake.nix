5189516fa6745ff4a4afa0c289d1ffd960d801de
Nathaniel Griswold <nategriswold@gmail.com>
2021-07-13 21:56:53 -0500

    blah

--- flake.nix
+++ flake.nix
@@ -1,14 +1,7 @@
 {
   description = "A very basic flake";
 
-  inputs.hierarchy.url = "github:nwg/flake-hierarchy-test-target-1";
-  inputs.abc.follows = "hierarchy/blah";
-  
-  outputs = { self, nixpkgs }: {
+  inputs.mytarget.url = "github:nwg/flake-hierarchy-test-target-1";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, ... }: {};
 }
